258431bbf9f86346b5949c3df6f98c3ac3b95563
Magdalene <magdalene809@gmail.com>
2019-06-29 22:59:27 -0500

    Created a cloud strife object and linked js file to main html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,17 @@
 // Objects
 
+var cloudStrife = {
+    firstName : "Cloud",
+    lastName: "Strife",
+    age : 21,
+    hp: 120,
+    attackPower: function (ap) {
+        ap += 2;
+
+    },
+    counterAttack: 10
+}
+
+console.log(cloudStrife.firstName);
+cloudStrife.attackPower(6);
+console.log(attackPower);
