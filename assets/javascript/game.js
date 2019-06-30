ee3d91cf58b586451a8238a931c8bd61b960d9c4
Magdalene <magdalene809@gmail.com>
2019-06-29 23:39:31 -0500

    Figured out the math for the attack power function in object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,19 @@ var cloudStrife = {
     lastName: "Strife",
     age : 21,
     hp: 120,
-    attackPower: function (ap) {
-        ap += 2;
+    calcAttackPower: function (ap) {
+        console.log('This is a test to see if Im calling the function correctly.');
+
+       var ap = 6;
+       return ap += ap;
+
 
     },
     counterAttack: 10
 }
-
+// cloudStrife.calcAttackPower();
+console.log(cloudStrife.calcAttackPower());
+// console.log(cloudStrife.calcAttackPower); this syntax prints the function 
+// console.log(cloudStrife.calcAttackPower()); this actuallyp performs the function but also gives an undefined reuslt, maybe because function is empty? 
 console.log(cloudStrife.firstName);
-cloudStrife.attackPower(6);
-console.log(attackPower);
+
