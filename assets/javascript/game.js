eb5fdc98ba61d97351885ed431fb7752112b6d7a
Magdalene <magdalene809@gmail.com>
2019-08-18 07:52:12 -0500

    Updated CSS, JS, and HTML to get program functioning correctly. Still need to make tweaks to battle system math and overall design/CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,118 +1,201 @@
 //Audio
-// The below doesn't do anything but I'm assuming it's because there are web browser things in place to prevent audio from auto playing. Even when using the audioElement.play() it doesnt load. It only works when it's within a jquery function. 
-var audioElement = new Audio("assets/theme.mp3");
-audioElement.autoplay = true;
-audioElement.load();
 
+// var audioElement = new Audio("assets/theme.mp3");
+// audioElement.autoplay = true;
+// audioElement.load();
 
-// Objects
-
-var characters = {
-
-    cloudStrife: {
-        firstName: "Cloud",
-        lastName: "Strife",
-        avatar: "assets/images/Cloud_Portrait.jpg",
-        age: 21,
-        hp: 120,
-        calcAttackPower: function (ap) {
-            console.log('This is a test to see if Im calling the function correctly.');
-
-            var ap = 12;
-            return ap += ap;
-        },
-        counterAttack: 10
-    },
-
-    tifaLockhart: {
-        firstName: "Tifa",
-        lastName: "Lockhart",
-        avatar: "assets/images/Tifa_Portrait.jpg",
-        age: 20,
-        hp: 100,
-        calcAttackPower: function (ap) {
-            var ap = 5;
-            return ap += ap;
-        },
-        counterAttack: 8
-    },
-
-    barretWallace: {
-        firstName: "Barret",
-        lastName: "Wallace",
-        avatar: "assets/images/Barret_Portrait.jpg",
-        age: 35,
-        hp: 130,
-        calcAttackPower: function (ap) {
-            var ap = 8;
-            return ap += ap;
-        },
-        counterAttack: 11
-    },
-
-    sephiroth: {
-        firstName: "Seiphiroth",
-        lastName: undefined,
-        avatar: "assets/images/Sephiroth_Portrait.jpg",
-        age: 27,
-        hp: 170,
-        calcAttackPower: function (ap) {
-            var ap = 20;
-            return ap += ap;
-        },
-        counterAttack: 15
-    }
-}
-
-var selectedCharacter; //this holds the selected character end user is playing as
-var enemiesAvailable= [] //this holds the remaining objects available to choose from as an enemy
-var defender; //this holds the selected enemy player has chosen from enemies available
-var charAvailable = characters; //this holds all my objects as its value is available characters to choose from 
-
-// Begin Jquery Code 
 $(document).ready(function () {
+    // Objects
+    var characters = {
 
-    $('#enemies-available').hide();
-    $('#enemy-character').hide();
+        "Cloud Strife": {
+            name: "Cloud Strife",
+            avatar: "assets/images/Cloud_Portrait.jpg",
+            age: 21,
+            hp: 120,
+            attack: 12,
+            counterAttack: 10
+        },
+
+        "Tifa Lockhart": {
+            name: "Tifa Lockhart",
+            avatar: "assets/images/Tifa_Portrait.jpg",
+            age: 20,
+            hp: 100,
+            attack: 5,
+            counterAttack: 8
+        },
+
+        "Barret Wallace": {
+            name: "Barret Wallace",
+            avatar: "assets/images/Barret_Portrait.jpg",
+            age: 35,
+            hp: 130,
+            attack: 8,
+            counterAttack: 11
+        },
+
+        "Sephiroth": {
+            name: "Sephiroth",
+            avatar: "assets/images/Sephiroth_Portrait.jpg",
+            age: 27,
+            hp: 170,
+            attack: 20,
+            counterAttack: 15
+        }
+    }
+
+    var selectedCharacter; //this holds the selected character end user is playing as
+    var enemiesAvailable = [] //this holds the remaining objects available to choose from as an enemy
+    var defender; //this holds the selected enemy player has chosen from enemies available
+    var turnCounter = 1;
+
+    var killCount = 0;
+    // var charAvailable = characters; //this holds all my objects as its value is available characters to choose from 
 
 
-
-    $('#Cloud').on('click', function () {
-        alert('Hey, you picked Cloud!');//simply testing to make sure the function works
-
-        // audioElement.play(); this works
-        
-
-        $('#Tifa').removeClass("main-characters").addClass("remaining-characters");
-        $('#Barret').removeClass("main-characters").addClass("remaining-characters");
-        $('#Sephiroth').removeClass("main-characters").addClass("remaining-characters");
-        $('#Tifa').appendTo(".remaining-character1");
-        $('#Barret').appendTo(".remaining-character2");
-        $('#Sephiroth').appendTo(".remaining-character3");
-        
-        $('#enemies-available').show();
-
-        //The above technically works as expected but the text doesn't move. I'm not quite worried about that because information from the object is supposed to be showing like the name property for example. But this is just another example of how I'm unsure on how to link objects together with html along with my jquery. 
+    var renderCharacter = function (character, renderArea) {
+        // This block of code builds the character card, and renders it to the page.
+        var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+        var charName = $("<div class='character-name'>").html(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr("src", character.avatar);
+        var charHealth = $("<div class='character-health'>").text(character.hp);
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(renderArea).append(charDiv);
+    };
 
 
+    //this loads the characters into the character section to be clicked
+
+    var initializeGame = function () {
+        //loop through the characters object and call the render character function so each show in section 
+
+        for (var key in characters) {
+            renderCharacter(characters[key], "#clickable-characters")
+        }
+    };
+
+    initializeGame();
+
+    //This function will render the potential enemies. 
+
+    var updateCharacter = function (charObj, areaRender) {
+        $(areaRender).empty();
+        renderCharacter(charObj, areaRender);
+    };
+
+    var renderEnemies = function (enemyArr) {
+        for (var i = 0; i < enemyArr.length; i++) {
+            renderCharacter(enemyArr[i], "#enemies-available")
+        }
+    };
+
+    var renderMessage = function (message) {
+        var gameMessageSet = $("#game-message");
+        var newMessage = $("<div>").text(message);
+        gameMessageSet.append(newMessage);
+
+    };
+
+    var restartGame = function (resultMessage) {
+        //What is the location reload? 
+        var restart = $("<button>Restart</button>").click(function () {
+            location.reload();
+        });
+
+        var gameState = $("<div>").text(resultMessage);
+
+        $("body").append(gameState);
+        $("body").append(restart);
+    };
+
+    var clearMessage = function () {
+        var gameMessage = $("#game-message");
+
+        gameMessage.text("");
+    };
+
+    $("#clickable-characters").on("click", ".character", function () {
+
+        var name = $(this).attr("data-name");
+        console.log("You clicked someone");
+        console.log(characters[name]);
+
+
+        if (!selectedCharacter) {
+            selectedCharacter = characters[name];
+
+            for (var key in characters) {
+                if (key !== name) {
+                    enemiesAvailable.push(characters[key]);
+                }
+            }
+
+            $("#clickable-characters").hide();
+
+            updateCharacter(selectedCharacter, "#selected-character");
+            renderEnemies(enemiesAvailable);
+             }
+        });
+
+
+    $("#enemies-available").on("click", ".character", function () {
+        var name = $(this).attr("data-name");
+
+        if ($("#defender").children().length === 0) {
+            defender = characters[name];
+            updateCharacter(defender, "#defender");
+
+            $(this).remove();
+            clearMessage();
+        }
     });
 
-      //The Cloud click function can be an example of how I expect the code to function and look from a front-end perspective. But how on earth can I do this for other characters? Let's say I wanted to select Tifa from remaining characters. Could I do an if/else statement? But how can I do an if-else statement when one of the arguments is a .click? Or if instead of playing Cloud I wanted to play as Sephiroth-- do I need if else using the above code? There has to be an eaiser way than that. 
+    $("#attack-button").on("click", function () {
+        if ($("#defender").children().length !== 0) {
+            var attackMessage = "You attacked " + defender.name + " for " + selectedCharacter.attack * turnCounter + " damage.";
+            var counterAttackMessage = defender.name + " attacked you back for " + defender.counterAttack + " damage";
+            clearMessage();
 
-      //For the most DRY code possible I need a function that recognizes that the images in the html are links to objects. Am I doing getElementById? Or perhaps I would do the attr() method somehow? 
+            defender.health -= selectedCharacter.attack * turnCounter;
 
+            if (defender.hp > 0) {
 
-     // audioElement.loop(); looping doesn't work regardless if this is in the jquery function or not
-})
+                updateCharacter(defender, "#defender");
 
+                renderMessage(attackMessage);
+                renderMessage(counterAttackMessage);
 
-//DEBUGGING AREA
+                selectedCharacter.hp -= defender.counterAttack;
 
-console.log(cloudStrife.calcAttackPower());
-console.log(tifaLockhart.calcAttackPower());
-console.log(barretWallace.calcAttackPower());
-console.log(sephiroth.calcAttackPower());
-// console.log(cloudStrife.calcAttackPower); this syntax prints the function 
-// console.log(cloudStrife.calcAttackPower()); this actually performs the function but also gives an undefined result, maybe because function param is empty? 
-console.log(cloudStrife.firstName);
+                updateCharacter(selectedCharacter, "#selected-character");
+
+                if (selectedCharacter.hp <= 0) {
+                    clearMessage();
+                    restartGame("You have been defeated...GAME OVER!!!");
+                    $("#attack-button").off("click");
+                }
+            }
+            else {
+                $("#defender").empty();
+
+                var gameStateMessage = "You have defeated " + defender.name + ", you  can choose to fight another enemy.";
+                renderMessage(gameStateMessage);
+
+                killCount++
+
+                if (killCount >= enemiesAvailable.length) {
+                    clearMessage();
+                    $("#attack-button").off("click");
+                    restartGame("You Won!!! GAME OVER!!!");
+                }
+            }
+            turnCounter++;
+        }
+        else {
+            clearMessage();
+            renderMessage("No enemy here.");
+        }
+    });
+});
 
